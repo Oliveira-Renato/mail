2013d035ed9651144d60b6442dc69d06af269e77
oliveira-renato <renato.printf@gmail.com>
2022-09-04 22:34:27 -0300

    New functionality: Mail content view

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -11,9 +11,45 @@ document.addEventListener('DOMContentLoaded', function() {
 
 });
 
+async function handleMailView(pMailID){
+  if(pMailID){
+    document.querySelector('#content-mail').innerHTML=''
+    try {
+      const mailID = await fetch(`/emails/${pMailID}`);
+      const result = await mailID.json()
+
+      if(result && !result.error){
+        console.log(result)
+        //Don't forget to make a function to hide the DIV's
+        document.querySelector('#emails-view').style.display ='none';
+        document.querySelector('#compose-view').style.display ='none';
+        document.querySelector('#content-mail').style.display ='block';
+
+        let mailBox = document.querySelector('#content-mail');
+        let div = document.createElement('div')
+        div.classList.add('mail-content')
+        div.innerHTML = `
+        <h2>${result['subject']}</h2>
+        <p>Sender: <strong>${result['sender']}</strong></p>
+        <div class="time-mail">${result['timestamp']}<p>
+        <div class="mail-body">
+          <p>${result['body']}</p>
+        </div>
+        <button>Reply</button>
+        `
+        mailBox.append(div)
+      }
+      
+    } catch (error) {
+      
+    }
+  }
+}
+
 function compose_email() {
 
   // Show compose view and hide other views
+  document.querySelector('#content-mail').style.display ='none';
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
@@ -50,45 +86,57 @@ function compose_email() {
   })
 }
 
-
 async function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#content-mail').style.display ='none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  const email= await fetch(`/emails/${mailbox}`);
-  const result = await email.json();
-  result && console.log(result);
-  if(result){ 
-    const body = document.querySelector('#emails-view')
-    const emails = result.sort()
-    let div = document.createElement('div')
-    div.classList.add(`box-${mailbox}`)
-    let ul =  document.createElement('ul')
-   
-    for(let email in emails) {
-      console.log(emails[email]['recipients'].length);
-      let li = document.createElement('li');
-      li.classList.add(`list-item`,`${emails[email]['read']?'bg-unread':'lol'}`) 
-      li.innerHTML = `
-        <div class="main-box-mail">
-          <p>${mailbox == 'inbox'?'Sender: ': 'To: '}
-            <strong>
-            ${mailbox == 'inbox'? emails[email]['sender']: (emails[email]['recipients'].length == 1 ? emails[email]['recipients'][0]:emails[email]['recipients'][0] +',...')}
-            </strong>
-          </p>
-          <p>Subject: <strong>${emails[email]['subject']}</strong></p>
-        </div>
-        <p>Date: <strong>${emails[email]['timestamp']}</strong></p>
-      `
-      ul.append(li)
-    }
-    div.append(ul)
-    body.append(div)
+  try {
+    const ajax= await fetch(`/emails/${mailbox}`);
+    const result = await ajax.json();
+    result && console.log(result);
+    if(result){ 
+      const body = document.querySelector('#emails-view')
+      const emails = result.sort()
+      let div = document.createElement('div')
+      div.classList.add(`box-${mailbox}`)
+      let ul =  document.createElement('ul')
+    
+      for(let email in emails) {
+        let li = document.createElement('li');
+        li.classList.add(`list-item`,`${emails[email]['read'] ?'bg-unread':'do-nothing'}`) 
+        li.setAttribute('data-id',`${emails[email]['id']}`)
+        li.innerHTML = `
+          <div class="main-box-mail">
+            <p>${mailbox == 'inbox'?'Sender: ': 'To: '}
+              <strong>
+              ${mailbox == 'inbox'? emails[email]['sender']: (emails[email]['recipients'].length == 1 ? emails[email]['recipients'][0]:emails[email]['recipients'][0] +',...')}
+              </strong>
+            </p>
+            <p>Subject: <strong>${emails[email]['subject']}</strong></p>
+          </div>
+          <p>Date: <strong>${emails[email]['timestamp']}</strong></p>
+        `
+        ul.append(li)
+      }
+      div.append(ul)
+      body.append(div)
 
+      document.querySelectorAll('[data-id]').forEach(node => {
+        node.addEventListener('click',(event)=>{
+          event.preventDefault()
+          handleMailView(node.dataset.id)
+        })
+      })
+    }else{
+      console.log('No results')
+    }
+  } catch (error) {
+    console.log(error)
   }
 }
